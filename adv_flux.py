aaa32ea0d40d9e4c1da36a0754e9dc5634a1a52d
Mussetti Gianluca <mugi@ddm04326.empa.emp-eaw.ch>
2018-03-14 11:16:47 +0100

    Cleaning after merging

--- adv_flux.py
+++ adv_flux.py
@@ -31,7 +31,7 @@ def point(ncd_name,lon,lat):
     u = ncd.variables['U'][0,-1,:,:]
     v = ncd.variables['V'][0,-1,:,:]
     t = ncd.variables['T'][0,-1,:,:]
-    w = ncd.variables['W'][0,-1,:,:]
+    #w = ncd.variables['W'][0,-1,:,:] only horizontal
     tup = ncd.variables['T'][0,-2,:,:]
     rho = ncd.variables['RHO'][0,-1,:,:]
     rhoup = ncd.variables['RHO'][0,-2,:,:]
@@ -61,8 +61,7 @@ def point(ncd_name,lon,lat):
     q_out_y = Ay2 * cps * ((rho[idy,idx]+rho[idy+1,idx])/2) *  \
              v[idy+1,idx] * ((t[idy,idx]+t[idy+1,idx])/2)
     Dq_tot = q_in_x - q_out_x + q_in_y - q_out_y # in W
-    Dq_vol = Dq_tot / (dx * dy * h) # volumetric (W/m3)
-    Dq_flux = Dq_vol * h  # W / m2 as other turbulent (unresolved) fluxes
+    Dq_flux = Dq_vol * (dx * dy)  # W / m2 as other turbulent (unresolved) fluxes
     # vertical component not considered
     #q_out_z = cps * ((rhoup[idy,idx]+rho[idy,idx])/2) *         \
     #         w[idy+1,idx] * ((tup[idy,idx]+t[idy,idx])/2)
@@ -76,21 +75,17 @@ def spatial(nc_name):
     # lat = geographical latitude
     # Outputs
     # dq = convective heat flux [W m-2]
-    # Hypotesis
-    # 1. t can be interpolated linearly
-    # 2. vertical advective fluxes neglected (need also w)
-    # constant (to be edited event.)
     #############################
     # constants
-    cps = 718 # J kg-1 K-1 at constant volume
-    ds = 280 # m
+    cps = 1000 # J kg-1 K-1 at constant pressure
+    ds = 278 # m approx. grid cell horizontal size (corresp to 0.0025°)
     # read grid variables
     nc = Dataset(nc_name,'r')
     vcoord = nc.variables['vcoord']
     # read variables        
     u = nc.variables['U'][0,-1,:,:]
     v = nc.variables['V'][0,-1,:,:]
-    #w = nc.variables['W'][0,-1,:,:]
+    #w = nc.variables['W'][0,-1,:,:] only horizontal
     t = nc.variables['T'][0,-1,:,:]
     tup = nc.variables['T'][0,-1,:,:]
     rho = nc.variables['RHO'][0,-1,:,:]
@@ -114,62 +109,9 @@ def spatial(nc_name):
             #q_out_z = cps * (rhoup[j,k]+rho[j,k])**0.5 *          \
             #         w[j,k] * t[j,k]
             Dq[j,k] = q_in_x - q_out_x + q_in_y - q_out_y # - q_out_z
+            # only horizontal fluxes
 
 
     return Dq
-=======
-    # advective flux
-    q_in_x = rho*cps*u[idy,idx]*t[idy,idx-1]
-    q_out_x = rho*cps*u[idy,idx+1]*t[idy,idx+1]
-    q_in_y = rho*cps*v[idy,idx]*t[idy-1,idx]
-    q_out_y = rho*cps*v[idy+1,idx]*t[idy+1,idx]
-    Dq = q_in_x - q_out_x + q_in_y - q_out_y
-    # convert to horizontal
-    h = vcoord[-2] - vcoord[-1]
-    Av = h * ds
-    Ah = ds**2
-    dh = Av/Ah
-    Dq_hor = Dq * dh
-    return Dq_hor
 
-def spatial(ncd_name):
-    ##########################
-    # Inputs
-    # ncd = path to netcdf file
-    # Outputs
-    # dq = convective heat flux [W m-2]
-    # Hypotesis
-    # - vertical advective fluxes neglected (need also w)
-    # constant (to be edited event.)
-    #############################
-    # constants
-    rho = 1 # kg m-3
-    cps = 1000 # J kg-1 K-1
-    ds = 280 # m
-    # read grid variables
-    ncd = Dataset(ncd_name,'r')
-    vcoord = ncd.variables['vcoord']
-    # read variables            
-    u = ncd.variables['U'][0,-1,:,:]
-    v = ncd.variables['V'][0,-1,:,:]
-    t = ncd.variables['T'][0,-1,:,:]
-    # define output variable
-    Dq = np.zeros((np.shape(u)[0]-1,np.shape(u)[1]-1))
-    # advective flux
-    for j in range(0, np.shape(u)[0]-1):
-        for k in range(0, np.shape(u)[1]-1):
-            q_in_x = rho*cps*u[j,k]*t[j,k-1]
-            q_out_x = rho*cps*u[j,k+1]*t[j,k+1]
-            q_in_y = rho*cps*v[j,k]*t[j-1,k]
-            q_out_y = rho*cps*v[j+1,k]*t[j+1,k]
-            Dq[j,k] = q_in_x - q_out_x + q_in_y - q_out_y
-
-
-    # convert to horizontal
-    h = vcoord[-2] - vcoord[-1]
-    Av = h * ds
-    Ah = ds**2
-    dh = Av/Ah
-    Dq_hor = Dq * dh
-    return Dq_hor
 
